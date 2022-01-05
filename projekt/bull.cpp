7be2de36e533d4f83134413d6f2aaeb0f4742b24
greger <emil9393@hotmail.com>
2022-01-05 05:53:18 -0500

    kuk

---
+++ projekt/bull.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <iostream>
+
+using namespace std;
+//sug min kuk
+int main(){
+    string num;
+
+    //inmatning av användare
+    cout << "Enter number" << endl;
+    cin >> num;
+
+
+
+    return 0;
+}
